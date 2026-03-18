8663d7d20f05941848e7368d685fb5c1b41fa742
CAPI <gonza_4_9@hotmail.com>
2026-03-18 12:16:06 +0100

    afegir git ignrore

--- obj/Debug/net10.0/Arkanoid.AssemblyInfo.cs
+++ obj/Debug/net10.0/Arkanoid.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arkanoid")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f91584f71216b5327fd4412a616a0f182396ed82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aaa389c1f0d7c878096312356bb781241218d15")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arkanoid")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arkanoid")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
